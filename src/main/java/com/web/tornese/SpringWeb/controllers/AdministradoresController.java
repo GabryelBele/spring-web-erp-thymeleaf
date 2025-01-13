6b4e5b9dbf075136caed1a421a3c8c70ec1d5ed0
GabryelBele <belegabryel19@gmail.com>
2025-01-13 16:41:59 -0300

    Add new admin

--- src/main/java/com/web/tornese/SpringWeb/controllers/AdministradoresController.java
+++ src/main/java/com/web/tornese/SpringWeb/controllers/AdministradoresController.java
@@ -1,12 +1,37 @@
 package com.web.tornese.SpringWeb.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.web.tornese.SpringWeb.models.Administrador;
+import com.web.tornese.SpringWeb.repositorio.AdministradoresRepo;
 
 @Controller
 public class AdministradoresController {
+
+  @Autowired
+  private AdministradoresRepo repo;
+
   @GetMapping("/administradores")
-  public String index(){
+  public String index(Model model){
+    List<Administrador> administradores = (List<Administrador>) repo.findAll();
+    model.addAttribute("administradores", administradores);
     return "administradores/index";
+  }
+
+  @GetMapping("/administradores/novo")
+  public String novo(){
+    return "administradores/novo";
+  }
+
+  @PostMapping("/administradores/criar")
+  public String criar(Administrador administrador){
+    repo.save(administrador);
+    return "redirect:/administradores";
   }
 }
