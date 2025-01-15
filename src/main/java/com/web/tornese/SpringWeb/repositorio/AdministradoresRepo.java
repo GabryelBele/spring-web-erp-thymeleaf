80d10bb0992da44929c78a47a9755f48fc7847a8
GabryelBele <belegabryel19@gmail.com>
2025-01-15 08:55:42 -0300

    Add event login page

--- src/main/java/com/web/tornese/SpringWeb/repositorio/AdministradoresRepo.java
+++ src/main/java/com/web/tornese/SpringWeb/repositorio/AdministradoresRepo.java
@@ -2,8 +2,11 @@ package com.web.tornese.SpringWeb.repositorio;
 
 import com.web.tornese.SpringWeb.models.Administrador;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AdministradoresRepo extends CrudRepository<Administrador, Integer> {
-  
+
+    @Query(value = "select * from administradores where email = :email and senha = :senha", nativeQuery = true)
+    public Administrador Login(String email, String senha);
 }
